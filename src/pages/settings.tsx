458bb77bfee486954a6113ea80263ac2d335462f
Nico <nico@nicos-mbp.home>
2022-08-16 22:05:57 +0200

    modified setting page

--- src/pages/settings.tsx
+++ src/pages/settings.tsx
@@ -1,3 +1,5 @@
+import ButtonWithIcon from '@/components/Buttons/ButtonWithIcon';
+import InsetNeumorphicContainer from '@/components/Containers/InsetNeumorphicContainer/InsetNeumorphicContainer';
 import PageContainerWithNav from '@/components/Containers/PageContainerWithNav/PageContainerWithNav';
 import FlexContainer from '@/components/FlexContainer/FlexContainer';
 import SectionTitle from '@/components/Texts/SectionTitle';
@@ -6,6 +8,7 @@ import { useAppDispatch, useAppSelector } from '@/redux/redux.hooks';
 import { setIsUserAuthenticated } from '@/redux/user/UserSlice';
 import { signOut } from 'firebase/auth';
 import { useRouter } from 'next/router';
+import LogoutIcon from '@mui/icons-material/Logout';
 import React from 'react';
 
 const SettingsPage = () => {
@@ -23,9 +26,15 @@ const SettingsPage = () => {
       <FlexContainer height='100px'>
         <SectionTitle title='Settings' color='white' />
       </FlexContainer>
-      <FlexContainer height='85vh'>
-        <h3>{email}</h3>
-        <button onClick={handleSignOut}>Log out</button>
+      <FlexContainer height='85vh' flexDirection='column'>
+        <h3 style={{ color: 'white' }}>Email: {email}</h3>
+        <ButtonWithIcon
+          title='Log out'
+          iconIsComponent
+          iconPosition='right'
+          icon={<LogoutIcon />}
+          onClick={handleSignOut}
+        />
       </FlexContainer>
     </PageContainerWithNav>
   );
